f9c53b5c9b38f63ad9b0501fe480229e24c62b9a
Carol <shunting.khor1@gmail.com>
2025-10-26 10:43:15 +0000

    Valid tag sequences as static const member

--- modules/tags.h
+++ modules/tags.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "sanitizer.h"
+#include <unordered_set>
 #include <vector>
 
 
 class TagCharSanitizer : public Sanitizer
 {
+private:
+    static const std::unordered_set<std::string> valid_sequences;
 public:
     void sanitize(std::vector<char32_t> &input) override;
 };
