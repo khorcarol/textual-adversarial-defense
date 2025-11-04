8dfcaf6bb3043080ab685ea8b891d6133273bbd4
Carol <shunting.khor1@gmail.com>
2025-11-04 23:13:13 +0000

    Added algorithm include

--- modules/variation_selector.cpp
+++ modules/variation_selector.cpp
@@ -10,6 +10,7 @@
 #include <filesystem>
 #include <fstream>
 #include <nlohmann/json.hpp>
+#include <algorithm>
 
 VariationSelectorSanitizer::VariationSelectorSanitizer(){
     std::filesystem::path source_dir = std::filesystem::path(__FILE__).parent_path().parent_path();
