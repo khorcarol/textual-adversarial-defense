40531e1c6c8496d5a2a86ee97379d7ccd93f045d
Carol <shunting.khor1@gmail.com>
2025-11-16 18:56:07 +0000

    Change homoglyph testing

--- tests/test_homoglyph.cpp
+++ tests/test_homoglyph.cpp
@@ -1,25 +1,36 @@
 #include <gtest/gtest.h>
 #include "homoglyph.h"
 
+// // One to One mappings sanitised
+// TEST(HomoglyphSanitizerTests, OneToOneMappings)
+// {
+//     HomoglyphSanitizer homoglyph_sanitizer;
+//     std::vector<char32_t> text = {0x0430, 0x03B1, 0xFF41}; // 'а', 'α', 'ａ', 'b'
+//     homoglyph_sanitizer.sanitize(text);
+
+//     std::vector<char32_t> expected = {0x0061, 0x0061, 0x0061}; // 'a', 'a', 'a', 'b'
+//     EXPECT_EQ(text, expected);
+// }
+
+// // One to Many mappings sanitised
+// TEST(HomoglyphSanitizerTests, OneToManyMappings)
+// {
+//     HomoglyphSanitizer homoglyph_sanitizer;
+//     std::vector<char32_t> text = {0x1F124}; // '⒤' (circled U)
+//     homoglyph_sanitizer.sanitize(text);
+
+//     std::vector<char32_t> expected = {0x0028, 0x0055, 0x0029}; // '(U)'
+//     EXPECT_EQ(text, expected);
+// }
+
 // One to One mappings sanitised
 TEST(HomoglyphSanitizerTests, OneToOneMappings)
 {
     HomoglyphSanitizer homoglyph_sanitizer;
-    std::vector<char32_t> text = {0x0430, 0x03B1, 0xFF41}; // 'а', 'α', 'ａ', 'b'
+    std::vector<char32_t> text = {0x0391, 0x001C3, 0x0392}; 
     homoglyph_sanitizer.sanitize(text);
 
-    std::vector<char32_t> expected = {0x0061, 0x0061, 0x0061}; // 'a', 'a', 'a', 'b'
-    EXPECT_EQ(text, expected);
-}
-
-// One to Many mappings sanitised
-TEST(HomoglyphSanitizerTests, OneToManyMappings)
-{
-    HomoglyphSanitizer homoglyph_sanitizer;
-    std::vector<char32_t> text = {0x1F124}; // '⒤' (circled U)
-    homoglyph_sanitizer.sanitize(text);
-
-    std::vector<char32_t> expected = {0x0028, 0x0055, 0x0029}; // '(U)'
+    std::vector<char32_t> expected = {0x0041, 0x0021, 0x0042}; 
     EXPECT_EQ(text, expected);
 }
 
